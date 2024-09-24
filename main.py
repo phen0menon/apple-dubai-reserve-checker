fbf03f5c7d2174d660369a982a3cd87895cf5ce1
phen0menon <roland.ibr@yahoo.com>
2024-09-24 23:59:23 +0200

    fix: remove testing code

--- main.py
+++ main.py
@@ -119,7 +119,7 @@ def get_models():
 
 def send_telegram_message(*, chat_id: str, bot_id: str, message: str) -> requests.Response:
     res = requests.get(f"https://api.telegram.org/bot{bot_id}/sendMessage?chat_id={chat_id}&text={message}")
-    if res.status_code == 200:
+    if res.status_code != 200:
         raise Exception(f"Failed to send telegram message: {res.text}")
     print("✔️\tMessage sent successfully")
 
